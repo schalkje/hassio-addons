4a099bb2432d2c52ed8bdfd31e5ca860c2ee2510
schalkje <jeroen_schalken@hotmail.com>
2018-12-12 21:28:00 +0100

    Added HelloWorld and MySensorsGateway

---
+++ HelloWorld/run.sh
@@ -0,0 +1,8 @@
+echo "Hello world!"
+echo ""
+echo "CONFIG:"
+echo "  CONFIG_PATH = '${CONFIG_PATH}'"
+echo "  APPDIR = '${APPDIR}'"
+echo ""
+NAME="$(jq --raw-output '.name' $CONFIG_PATH)"
+echo "Hello $NAME."
---
+++ MySensorsGateway/README.md
@@ -0,0 +1,7 @@
+# hassio-addons
+Addons for Hass.io
+
+mysgw - MySensors Gateway. MQTT only, for now!
+
+## My home
+Trying to get it running on my pi
