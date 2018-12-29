49e9a840017272bf110950fd668e68136e40d81e
Jeroen Schalken <jeroen_schalken@hotmail.com>
2018-12-29 15:18:43 +0100

    Add installation of gcc and make Add config file checker

---
+++ MySensorsGateway/rootfs/etc/cont-init.d/20-config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# This copies the configs to their respective locations
+# ==============================================================================
+# shellcheck disable=SC1091
+source /usr/lib/hassio-addons/base.sh
+
+config_file="/config/$(hass.config.get 'config_file')"
+
+if [ ! -f "$config_file" ]; then
+  hass.log.info "Config file does not exist. Creating.."
+  cp /etc/home-panel/home-panel-config.default.json "$config_file"
+  hass.log.info "Created. You should now edit this file at '$config_file'"
+fi 
