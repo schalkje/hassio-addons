6d9682b27b5ea94894904219330687b0acd3c9c5
Jeroen Schalken <jeroen_schalken@hotmail.com>
2019-09-04 14:44:34 +0200

    Change logging from hass. --> bashio:

--- example/rootfs/usr/bin/run.sh
+++ example/rootfs/usr/bin/run.sh
@@ -28,7 +28,7 @@ get_quote_online() {
     local html
     local quote
 
-    hass.log.trace "${FUNCNAME[0]}"
+    bashio::log.trace "${FUNCNAME[0]}"
 
     number=$(( ( RANDOM % 999 )  + 1 ))
     html=$(wget -q -O - "http://www.quotationspage.com/quote/${number}.html")
@@ -57,7 +57,7 @@ get_quote_offline() {
     local -i number
     local -a quotes
 
-    hass.log.trace "${FUNCNAME[0]}"
+    bashio::log.trace "${FUNCNAME[0]}"
 
     quotes+=("Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.\\n -Samuel Beckett")
     quotes+=("Never give up, for that is just the place and time that the tide will turn.\\n -Harriet Beecher Stowe")
@@ -87,7 +87,7 @@ display_quote() {
     local quote
     local timestamp
 
-    hass.log.trace "${FUNCNAME[0]}"
+    bashio::log.trace "${FUNCNAME[0]}"
 
     if wget -q --spider http://www.quotationspage.com; then
         quote=$(get_quote_online)
@@ -100,7 +100,7 @@ display_quote() {
     quote=$(sed 's/n()//g' <<< "${quote}" | xargs -0 echo | fmt -40)
     timestamp=$(date +"%T")
 
-    hass.log.info "Random quote loaded @ ${timestamp}"
+    bashio::log.info "Random quote loaded @ ${timestamp}"
     echo -e "${quote}"
 }
 
@@ -110,10 +110,10 @@ display_quote() {
 main() {
     local sleep
 
-    hass.log.trace "${FUNCNAME[0]}"
+    bashio::log.trace "${FUNCNAME[0]}"
 
     sleep=$(hass.config.get 'seconds_between_quotes')
-    hass.log.info "Seconds between each quotes is set to: ${sleep}"
+    bashio::log.info "Seconds between each quotes is set to: ${sleep}"
 
     while true; do
         display_quote
