fc8a7ac3f735e3ba2be5aa7c0f86986fc9f08279
Jeroen Schalken <jeroen_schalken@hotmail.com>
2019-09-28 18:06:58 +0200

    switch bash to bashio; clean files

--- MySensorsGateway/rootfs/etc/cont-init.d/20-config.sh
+++ MySensorsGateway/rootfs/etc/cont-init.d/20-config.sh
@@ -1,21 +1,30 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Ensures the configuration for the user exists
 # ==============================================================================
+# example code: 
+#   https://github.com/hassio-addons/addon-vscode/blob/master/vscode/rootfs/etc/cont-init.d/user.sh for bashio
+#   https://github.com/hassio-addons/bashio
 # shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
 
 # Ensure configuration exists
-if ! hass.directory_exists '/config/mysensors/'; then
+if ! bashio::fs.directory_exists '/config/mysensors/'; then
+    mkdir -p /config/mysensors \
+        || hass.die "Failed to create mysensors configuration folder"
+fi
+
+if ! bashio::fs.directory_exists '/config/mysensors/devices'; then
     mkdir -p /config/mysensors/devices \
-        || hass.die "Failed to create mysensors configuration directory"
-
-    # Copy in template files
-    cp /etc/mysensors/mysensors_devices.json /config/mysensors/
-
+        || hass.die "Failed to create mysensors devices folder"
 fi
 
 # create initial config file
-if ! hass.file_exists '/config/mysensors/mysensors.conf'; then
+if ! bashio::fs.file_exists '/config/mysensors/mysensors_devices.json'; then
+    # Copy in template files
+    cp /etc/mysensors/mysensors_devices.json /config/mysensors/
+fi
+
+# create initial config file
+if ! bashio::fs.file_exists '/config/mysensors/mysensors.conf'; then
     cp /etc/mysensors/mysensors.conf /config/mysensors/
 fi
