7ef08ab2deb24022a7d25518c4c0a6fdae4488ee
schalkje <jeroen_schalken@hotmail.com>
2019-10-27 19:11:03 +0100

    Quote the password

--- MySensorsGateway/rootfs/etc/cont-init.d/20-config.sh
+++ MySensorsGateway/rootfs/etc/cont-init.d/20-config.sh
@@ -8,23 +8,23 @@
 # shellcheck disable=SC1091
 
 # Ensure configuration exists
-if ! bashio::fs.directory_exists '/config/mysensors/'; then
+if !bashio::fs.directory_exists '/config/mysensors/'; then
     mkdir -p /config/mysensors \
         || hass.die "Failed to create mysensors configuration folder"
 fi
 
-if ! bashio::fs.directory_exists '/config/mysensors/devices'; then
+if !bashio::fs.directory_exists '/config/mysensors/devices'; then
     mkdir -p /config/mysensors/devices \
         || hass.die "Failed to create mysensors devices folder"
 fi
 
 # create initial config file
-if ! bashio::fs.file_exists '/config/mysensors/mysensors_devices.json'; then
+if !bashio::fs.file_exists '/config/mysensors/mysensors_devices.json'; then
     # Copy in template files
     cp /etc/mysensors/mysensors_devices.json /config/mysensors/
 fi
 
 # create initial config file
-if ! bashio::fs.file_exists '/config/mysensors/mysensors.conf'; then
+if !bashio::fs.file_exists '/config/mysensors/mysensors.conf'; then
     cp /etc/mysensors/mysensors.conf /config/mysensors/
 fi
